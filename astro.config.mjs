f966614104672da220efbfa7e8335099202a1397
Niranjan-GopaL <niranjan.gopal@iiitb.ac.in>
2026-06-09 22:02:58 +0530

    Github pages temporarily; Astro 6 requires Node 22 for actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+// export default defineConfig({
+//   site: 'https://realaspectratiocalculate.com',
+//   vite: {
+//     plugins: [tailwindcss()],
+//   },
+// });
+
+
 export default defineConfig({
-  site: 'https://realaspectratiocalculate.com',
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: 'https://niranjan-gopal.github.io',
+  base: '/realaspectratiocalculate.com',
+    output: 'static',
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
